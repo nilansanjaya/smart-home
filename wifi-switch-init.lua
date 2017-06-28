6449bf0ad62c45271e2b3169357d876f8e110441
Nilan Sanjaya <nilansanjaya@gmail.com>
2017-06-28 21:32:41 +0530

    Update nodemcu device logic

--- wifi-switch-init.lua
+++ wifi-switch-init.lua
@@ -1,33 +1,50 @@
-wifi.setmode(wifi.STATION)
-wifi.sta.config("WIFI_SSID","WIFI_PASSWORD")
---print(wifi.sta.status())
---print(wifi.sta.getip())
+wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
+
+http.get("http://192.168.1.9:3000/api/register/1942035/LIGHT/"..T.IP, nil, function(code, data)
+    
+  end)
+
+collectgarbage();
+
+tmr.delay(100);
+  
 led1 = 0
 gpio.mode(led1, gpio.OUTPUT)
+
+tmr.alarm(0, 1000, 1, function()
+
 srv=net.createServer(net.TCP)
 srv:listen(80,function(conn)
     conn:on("receive", function(client,request)
-        local buf = "";
         local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
-        if(method == nil)then
-            _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP");
-        end
+        --if(method == nil)then
+        --    _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP");
+        --end
         local _GET = {}
         if (vars ~= nil)then
             for k, v in string.gmatch(vars, "(%w+)=(%w+)&*") do
                 _GET[k] = v
             end
         end
-        buf = buf.."<h1> ESP8266 Web Server</h1>";
-        buf = buf.."<p>GPIO0 <a href=\"?pin=ON1\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF1\"><button>OFF</button></a></p>";
-        local _on,_off = "",""
+        
         if(_GET.pin == "ON1")then
               gpio.write(led1, gpio.LOW);
         elseif(_GET.pin == "OFF1")then
               gpio.write(led1, gpio.HIGH);
         end
-        --client:send(buf);
+        
         client:close();
         collectgarbage();
     end)
 end)
+
+tmr.stop(0);
+
+end)
+
+ end)
+
+station_cfg={}
+station_cfg.ssid="SSID"
+station_cfg.pwd="PASSWORD"
+wifi.sta.config(station_cfg)
